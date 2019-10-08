0ebfda62809fa01f8d97d4d7109f79af84704701
Rafael Calhau <rafael@calhau.me>
2019-10-08 11:30:41 -0300

    useHistory hook added

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,18 +1,19 @@
 import React, { ReactElement } from 'react'
 import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 import ExitToApp from '@material-ui/icons/ExitToApp'
 import { logoutUser } from '../store/actions/user'
 
 export default function UserMenu (): ReactElement {
   const dispatch = useDispatch()
-  const logout = (): void => {
-    dispatch(logoutUser())
-  }
+  const history = useHistory()
+  const logout = (): void => { dispatch(logoutUser()) }
+  const profile = (): void => { history.push('/profile') }
 
   return (
     <nav className="usernav">
-      <button>
+      <button onClick={profile}>
         <AccountCircle />
         Profile
       </button>
