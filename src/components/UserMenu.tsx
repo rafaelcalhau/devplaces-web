74fb581aef5faa91ffd1bf2b334e1270f26b0b29
Rafael Calhau <rafael@calhau.me>
2019-10-08 13:14:45 -0300

    Added the dashboard link to menu.

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -3,16 +3,22 @@ import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 import ExitToApp from '@material-ui/icons/ExitToApp'
+import HomeIcon from '@material-ui/icons/Apps'
 import { logoutUser } from '../store/actions/user'
 
 export default function UserMenu (): ReactElement {
   const dispatch = useDispatch()
   const history = useHistory()
+  const dashboard = (): void => { history.push('/') }
   const logout = (): void => { dispatch(logoutUser()) }
   const profile = (): void => { history.push('/profile') }
 
   return (
     <nav className="usernav">
+      <button onClick={dashboard}>
+        <HomeIcon />
+        Dashboard
+      </button>
       <button onClick={profile}>
         <AccountCircle />
         Profile
